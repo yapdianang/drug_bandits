902b597b1258ef081b8dc24318166b3146954ec3
justinxu421 <justinx@stanford.edu>
2019-03-10 00:45:17 -0800

    got .65 accuracy ye

--- model/linearUCB.py
+++ model/linearUCB.py
@@ -1,6 +1,6 @@
 import numpy as np
 from utils import get_data
-
+from collections import defaultdict
 
 class DataStream(object):
     # Read in a csv, shuffle rows.
@@ -8,8 +8,8 @@ class DataStream(object):
     # For each row:
         # yield feature vector extracted from that row, and ground truth action
 
-    def __init__(self, csv_path):
-        self.table, self.ground_truth = get_data(csv_path)
+    def __init__(self, csv_path, seed=234):
+        self.table, self.ground_truth = get_data(csv_path, seed)
         self.max_rows = len(self.table)
         self.feature_dim = self.table.shape[-1]
         self.current = 0
@@ -23,8 +23,9 @@ class DataStream(object):
         if self.current >= self.max_rows:
             raise StopIteration
         else:
+            output = (self.table[self.current], self.ground_truth[self.current])
             self.current += 1
-            return (self.table[self.current], self.ground_truth[self.current])
+            return output
 
 
 
@@ -45,17 +46,15 @@ class LinearUCBBandit(object):
                 d := dimension of the feature vectors
                 delta := choice of parameter for asymptotic convergence guarantees
         """
-        self.alpha = alpha(delta, T, K)
+        self.alpha = 0.1 
         self.K = K
         self.d = d
-        self.A = np.eye(self.d, dtype=np.float64) # (d,d) identity matrix
-        self.b = np.zeros((self.d,)) # (d,) vector
-        self.theta = None
-
+        self.A = [np.eye(self.d, dtype=np.float64) for _ in range(K)] # (d,d) identity matrix
+        self.b = [np.zeros((self.d,)) for _ in range(K)] # (d,) vector
 
     # At some timestep t, we get a new action
     def get_action(self, features, ground_truth_action):
-        
+        features = features.astype(int) 
         # Translate the strings "low", "medium", "high" into (0,1,2) resp.
         if ground_truth_action == "low":
             ground_truth_action = 0
@@ -66,42 +65,54 @@ class LinearUCBBandit(object):
         else:
             raise ValueError("ground_truth_action: not one of 'low', 'medium', 'high'")
 
-
-        self.theta = np.linalg.pinv(self.A).dot(self.b)  # pseudo-inverse if noninvertible A, otherwise inverse
-
         # features_k is just features (we don't have diff features for each of K actions)
         all_upper_bounds = []
         for action in range(self.K):
-            xT_A_x = features.T.dot(np.linalg.pinv(self.A)).dot(features)
+            A,b = self.A[action], self.b[action]
+            theta = np.linalg.pinv(A).dot(b)  # pseudo-inverse if noninvertible A, otherwise inverse
+            xT_A_x = features.T.dot(np.linalg.pinv(A)).dot(features)
             ucb_variance = self.alpha * np.sqrt(xT_A_x)
-            upper_bound = self.theta.T.dot(features) + ucb_variance
+            upper_bound = theta.dot(features) + ucb_variance
             all_upper_bounds.append(upper_bound)
 
-        # Choose action, observe payoff
-        reward = 0. if action == ground_truth_action else -1.
-        regret = 0 - reward # optimal reward is always 0 (correct dosage given)
         best_action = np.random.choice([i for i,p in enumerate(all_upper_bounds) if p == np.amax(all_upper_bounds)])
+        # best_actions = [i for i,p in enumerate(all_upper_bounds) if p == np.amax(all_upper_bounds)]
+        # if 1 in best_actions: 
+        #     best_action = 1
+        # else:
+        #     best_action = np.random.choice(best_actions)
+
+        # Choose action, observe payoff
+        reward = 0. if best_action == ground_truth_action else -1.
+        regret = 0 - reward # optimal reward is always 0 (correct dosage given)
 
         # Update
-        self.A = self.A + features.dot(features.T)
-        self.b = self.b + features * reward
+        self.A[best_action] = self.A[best_action] + np.outer(features, features)
+        self.b[best_action] = self.b[best_action] + features * reward
 
         return best_action, reward, regret
 
 
 if __name__ == "__main__":
     # Run the stream of data through LinearUCBBandit, and get regret and prediction accuracy
-    ds = DataStream("../data/warfarin.csv")
-    delta = 0.1
-    bandit = LinearUCBBandit(ds.max_rows, 3, ds.feature_dim, delta)
+    seeds = range(10)
+    for seed in seeds: 
+        ds = DataStream("../data/warfarin.csv", seed=seed)
+        delta = 0.75
+        bandit = LinearUCBBandit(ds.max_rows, 3, ds.feature_dim, delta)
+        print(ds.feature_dim, bandit.alpha)
 
-    total_regret = 0
-    nb_correct = 0
-    for features, ground_truth_action in ds:
-        best_action, reward, regret = bandit.get_action(features, ground_truth_action)
-        total_regret += regret
-        nb_correct += 1 if (reward == 0) else 0
-    accuracy = nb_correct / ds.max_rows
-
+        total_regret = 0
+        nb_correct = 0
+        actions = defaultdict(int)
+        for features, ground_truth_action in ds:
+            best_action, reward, regret = bandit.get_action(features, ground_truth_action)
+            actions[best_action] += 1
+            total_regret += regret
+            nb_correct += 1 if (reward == 0) else 0
+        accuracy = nb_correct / ds.max_rows
+        print(actions)
+        print(accuracy, total_regret)
+            
 
 
--- model/utils.py
+++ model/utils.py
@@ -26,6 +26,9 @@ def get_data(path, seed=234):
 	df = pd.read_csv(path)
 	df = df.dropna(subset = ['Therapeutic Dose of Warfarin'])
 	df['dosage_bucket'] = (df['Therapeutic Dose of Warfarin'] / 7).apply(get_bucket)
+	df['Carbamazepine (Tegretol)'] = df['Carbamazepine (Tegretol)'].astype('object')
+	df['Phenytoin (Dilantin)'] = df['Phenytoin (Dilantin)'].astype('object')
+	df['Rifampin or Rifampicin'] = df['Rifampin or Rifampicin'].astype('object')
 
 	feature_list = ['Gender', 'Race', 'Ethnicity', 'Age', 'Cyp2C9 genotypes', \
             'VKORC1 genotype: -1639 G>A (3673); chr16:31015190; rs9923231; C/T', \
@@ -34,7 +37,17 @@ def get_data(path, seed=234):
             'VKORC1 genotype: 1542G>C (6853); chr16:31012010; rs8050894; C/G', \
             'VKORC1 genotype: 3730 G>A (9041); chr16:31009822; rs7294;  A/G', \
             'VKORC1 genotype: 2255C>T (7566); chr16:31011297; rs2359612; A/G', \
-            'VKORC1 genotype: -4451 C>A (861); Chr16:31018002; rs17880887; A/C']
+            'VKORC1 genotype: -4451 C>A (861); Chr16:31018002; rs17880887; A/C', \
+            ]
+	# feature_list = ['Gender', 'Cyp2C9 genotypes', 
+ #            'VKORC1 genotype: -1639 G>A (3673); chr16:31015190; rs9923231; C/T', \
+ #            'VKORC1 genotype: 497T>G (5808); chr16:31013055; rs2884737; A/C', \
+ #            'VKORC1 QC genotype: 1173 C>T(6484); chr16:31012379; rs9934438; A/G', \
+ #            'VKORC1 genotype: 1542G>C (6853); chr16:31012010; rs8050894; C/G', \
+ #            'VKORC1 genotype: 3730 G>A (9041); chr16:31009822; rs7294;  A/G', \
+ #            'VKORC1 genotype: 2255C>T (7566); chr16:31011297; rs2359612; A/G', \
+ #            'VKORC1 genotype: -4451 C>A (861); Chr16:31018002; rs17880887; A/C']
+	# feature_list = ['Gender']
 	features = pd.get_dummies(df[feature_list], dummy_na=True)
 	features['bias'] = 1
 	features['dosage_bucket'] = df['dosage_bucket']
