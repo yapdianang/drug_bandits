a635bddbf98563ea4951cb7d4aa483db3f02815a
Owen Wang <owenwang0328@gmail.com>
2019-03-14 17:08:55 -0700

    Moved DataStream(object) into utils, started lassoBandit

--- model/linearUCB.py
+++ model/linearUCB.py
@@ -1,50 +1,12 @@
 import numpy as np
 import pandas as pd
-from utils import get_data
+from utils import get_data, DataStream
 from collections import defaultdict
 from plot import plot_
 from sklearn.model_selection import train_test_split
 import argparse
 import pickle
 import os
-
-
-class DataStream(object):
-    # Read in a csv, shuffle rows.
-    # Iterator below.
-    # For each row:
-        # yield feature vector extracted from that row, and ground truth action
-
-    def __init__(self, csv_path, seed=234):
-        features, dosage = get_data(csv_path, seed)
-
-        self.table, self.table_test, y, y_test = \
-                train_test_split(features, dosage, test_size=0.1, random_state=seed, stratify=dosage[:,0])
-
-        self.ground_truth, self.dosage = y[:,0], y[:,1]
-        self.ground_truth_test, self.dosage_test = y_test[:,0], y_test[:,1]
-
-        self.max_rows = len(self.table)
-        self.feature_dim = self.table.shape[-1]
-        self.current = 0
-
-    # Iterator methods
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.current >= self.max_rows:
-            raise StopIteration
-        else:
-            # This line determines discrete buckets vs. floating point dosages #######################################################
-
-            # Depends on what Justin's csv columns contain
-
-            output = (self.table[self.current], self.ground_truth[self.current], self.dosage[self.current]) 
-            self.current += 1
-            return output
-
 
 
 def alpha(delta, T, K):
@@ -212,6 +174,7 @@ if __name__ == "__main__":
     plot_(x_vals, incorrect_accuracy_over_runs, args.mode, 'percent_incorrect') 
     plot_(x_vals, regret_over_runs, args.mode, 'regret') 
     plot_(x_vals, risk_over_runs, args.mode, 'risk') 
+    
     # Store our accuracies and regret
     """
     prefix = "linearUCB_" + args.mode
